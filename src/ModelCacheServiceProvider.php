e8a871fbfedd48e3ab09aed8a95f4537518833f3
Kurt Friars <kfriars@apple.com>
2024-06-10 09:18:12 -0400

    Adjust dependencies to work across Laravel versions

--- src/ModelCacheServiceProvider.php
+++ src/ModelCacheServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Plank\ModelCache;
 
+use Spatie\LaravelPackageTools\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
@@ -11,6 +12,21 @@ class ModelCacheServiceProvider extends PackageServiceProvider
     {
         $package
             ->name('model-cache')
-            ->hasConfigFile();
+            ->hasConfigFile()
+            ->hasInstallCommand(function (InstallCommand $command) {
+                $command->startWith(function (InstallCommand $command) {
+                    $command->info("🗄️ Laravel Model Cache Installer... \n");
+
+                    if ($command->confirm('Would you like to publish the config file?')) {
+                        $command->publishConfigFile();
+                    }
+                });
+
+                $command->endWith(function (InstallCommand $command) {
+                    $command->info('✅ Installation complete.');
+
+                    $command->askToStarRepoOnGitHub('plank/model-cache');
+                });
+            });
     }
 }
